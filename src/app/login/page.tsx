4a42fc104bd2300c89148b1cef68c7e32379f669
Ayaz Hussain <ayazhussain.bssef22@iba-suk.edu.pk>
2025-07-31 17:08:42 +0500

    update

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -48,7 +48,8 @@ export default function LoginPage() {
     const { error } = await supabase.auth.signInWithOtp({
       email,
       options: {
-        emailRedirectTo: `${window.location.origin}/auth/callback`,
+        // emailRedirectTo: `${window.location.origin}/auth/callback`,
+        emailRedirectTo: `https://ahresumetrailer.vercel.app/createresume`,
       },
     });
 
